caf36d42b01bd448239fc26d556d0dd0610bf207
Joel Bremner <jbr257@uclive.ac.nz>
2024-08-28 22:41:23 +1200

    Added REST Controller for hello world

---
+++ api/src/main/java/com/clubhub/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.clubhub.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * A hello world controller to help get set up
+ */
+@RestController
+public class HomeController {
+
+    /**
+     * Constructor for class
+     * This is where all Auto wired dependencies go
+     */
+    public HomeController() {}
+
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+
+}
