e79161ad4e440bdfe9c6593a78b977634aa6c13b
Joel Bremner <jbr257@uclive.ac.nz>
2024-08-31 23:15:39 +1200

    Fixed infinite depth recursion with clubs and unions.

--- api/src/main/java/com/clubhub/entity/Union.java
+++ api/src/main/java/com/clubhub/entity/Union.java
@@ -1,5 +1,6 @@
 package com.clubhub.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,5 +28,6 @@ public class Union {
             name = "union_clubs",
             joinColumns = @JoinColumn(name = "union_id"),
             inverseJoinColumns = @JoinColumn(name = "club_id"))
+    @JsonIgnore
     private List<Club> clubs;
 }
