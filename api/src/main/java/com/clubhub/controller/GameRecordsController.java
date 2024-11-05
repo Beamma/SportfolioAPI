90bb790fc7aaa57dc83412b07b622fd2181cf283
Joel Bremner <jbr257@uclive.ac.nz>
2024-11-05 21:45:41 +1300

    Remove debug print statement

--- api/src/main/java/com/clubhub/controller/GameRecordsController.java
+++ api/src/main/java/com/clubhub/controller/GameRecordsController.java
@@ -45,7 +45,6 @@ public class GameRecordsController {
                                                @RequestBody List<AddBulkSeasonRequest> bulkSeasonsRequest,
                                                HttpServletRequest request) {
         System.out.println("POST /clubs/{clubId}/statistics/season");
-        System.out.println(bulkSeasonsRequest.get(0).season);
 
         // Create Response
         Map<String, Object> response =  new HashMap<>();
